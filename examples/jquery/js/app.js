01e8f4e86ab4594b2eaa6b9ac0b4fccb676fa3cf
donli1210 <donli1210@gmail.com>
2017-12-03 09:20:09 -0600

    Change git user

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -5,6 +5,7 @@ jQuery(function ($) {
 	Handlebars.registerHelper('eq', function (a, b, options) {
 		return a === b ? options.fn(this) : options.inverse(this);
 	});
+	
 
 	var ENTER_KEY = 13;
 	var ESCAPE_KEY = 27;
